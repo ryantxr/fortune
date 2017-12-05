278902da7266bddbea4fea249ed4c6e0251c5561
ryantxr <ryantxr@gmail.com>
2017-12-05 13:56:29 -0500

    Getting going.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # fortune
+
 A PHP Fortune class
 This is a simple class that will return one of a set of "fortunes".
---
+++ app/Ryantxr/Fortune/Fortune.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ryantxr\Fortune;
+
+class Fortune {
+
+    public function fortune() {
+        return "Be cool, but also be warm.";
+    }
+
+}
